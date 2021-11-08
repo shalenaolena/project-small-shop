32c79596ecc499a126414e51a6c1730ceeb084fc
shalenaolena <shalenaolena@gmail.com>
2021-11-08 08:51:24 +0200

    edit

--- scripts/shop.js
+++ scripts/shop.js
@@ -7,6 +7,16 @@ class Shop {
         this.arrItemsObj = arrItemsObj;
     }
 
+    showMessage(massege, time) {
+
+        $("#modal_massege_id, .modal_massege_overlay").fadeIn();
+        $("#modal_massege_text").text(massege);
+
+        setTimeout( () => {
+            $(".modal_massege_overlay, #modal_massege_id").fadeOut();
+        }, time);  
+    }
+
     shopTitle () {
         document.getElementById("logo_text").innerHTML = this.shopName;     //Назва магазину
     }
@@ -17,6 +27,13 @@ class Shop {
             modalWin.fadeIn();
         });
     }
+
+    openModalWinWithOutEvent(modalWin) {
+        $("#modal_overlay_id").fadeIn();
+            modalWin.fadeIn();
+
+    }
+
     closeModalWindows () {
         $(".modal_close").on("click", function () {         //закриття модальних вікон хрестиком
             $('.modal_win').fadeOut();                      //#modal_overlay_id, #modal_item_add_edit, #modal_filter_id
@@ -33,7 +50,7 @@ class Shop {
 
     setDataToLocalStorage (key, itemToAdd) {                //записати дані у localStorage
         localStorage.setItem(key, JSON.stringify(itemToAdd));
-            alert(`Додадно новий елемент у ${key}`);
+            this.showMessage(`Внесено зміни у ${key}!`, 2000);
             let raw = localStorage.getItem(key);
             let result = JSON.parse(raw);
             console.log(result);
@@ -44,7 +61,9 @@ class Shop {
 
     formData() {
         this.getDataFromLocalStorage ("arrItems"); 
-        alert(`Hello! We get ${this.arrItems.length} items in Local Storage! xoxo`); //повідомлення - актуальна кількіть товарів
+       
+        //alert(`Hello! We get ${this.arrItems.length} items in Local Storage! xoxo`); 
+        //повідомлення - актуальна кількіть товарів
 
         $("#modal_item_add_edit_btn").on("click", () => {       
 
@@ -60,27 +79,40 @@ class Shop {
             let result = new Items (itemIdData, itemNameData, itemCategoryData, itemPriceData, itemImgUrlData, itemDescriptionData);
             this.arrItems.push(result);
             this.setDataToLocalStorage ("arrItems", this.arrItems);
+            this.getDataFromLocalStorage ("arrItems");
+            this.parseArrToClassItems (this.arrItems);
+
         }) 
     }
 
-    parseArrToClassItems (arr) {
-        for (let i = 0; i < arr.length; i++) {
-            let newItem = new Items (arr[i]["itemId"], 
-            arr[i].itemName, 
-            arr[i].itemCategory, 
-            arr[i].itemPrice, 
-            arr[i].itemImg, 
-            arr[i].itemDescription);
+    parseArrToClassItems () {
+        for (let i = 0; i < this.arrItems.length; i++) {
+            let newItem = new Items (this.arrItems[i]["itemId"], 
+            this.arrItems[i].itemName, 
+            this.arrItems[i].itemCategory, 
+            +this.arrItems[i].itemPrice, 
+            this.arrItems[i].itemImg, 
+            this.arrItems[i].itemDescription);
             this.arrItemsObj.push(newItem);
         }
     }
 
-  
+    calcSum() {
+        let result = 0;
+        for (let i = 0; i < this.arrItems.length; i++) {
+            result += +this.arrItems[i]["itemPrice"];
+        }
+        return result;
+        
+    }
+
+    
    
 }
 
 
 //Класс товаров
+
 class Items extends Shop {
     constructor (itemId, itemName, itemCategory, itemPrice, itemImg, itemDescription) {
         super ();
@@ -90,6 +122,9 @@ class Items extends Shop {
         this.itemPrice = itemPrice;
         this.itemImg = itemImg;
         this.itemDescription = itemDescription;
+        this.arrItems;
+        this.arrItemsObj;
+
     }
 
     loadCatalog (arr) {
@@ -98,52 +133,186 @@ class Items extends Shop {
             document.querySelector("#container_items_catalog").insertAdjacentHTML("afterbegin",
 
                 `<div class="item" id="${i}">
-            <div class="btn_item_edit">
-                <img src="icons/edit.png" alt="">
-            </div>
-            <img src="${arr[i].itemImg}" alt="">
-            <div class="item_data_blok">
-                <div class="item_name">
-                    <h3>${arr[i].itemName}</h3>
-                    <h4 class = "item_price">Ціна: ${arr[i].itemPrice} грн</h4>
-                </div>
-                <div class="item_btns">
-                    <div class="item_btn_more_info">
-                        Деталі
+                    <img src="${arr[i].itemImg}" alt="">
+                    <div class="item_data_blok">
+                        <div class="item_name">
+                            <h3>${arr[i].itemName}</h3>
+                            <h4 class = "item_price">Ціна: ${arr[i].itemPrice} грн</h4>
+                        </div>
+                        <div class="item_btns">
+                            <div class="item_btn_more_info" data-btn-open="${arr[i].itemId}" id="open${arr[i].itemId}">
+                                Деталі
+                            </div>
+                            <div class="item_btn_add_to_cart">
+                                    <img src="icons/white-cart.png" alt="">
+                            </div>
+                        </div>
                     </div>
-                    <div class="item_btn_add_to_cart">
-                            <img src="icons/white-cart.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div> <!--item -->`
+                </div> <!--item -->`
 
-            );
+                // <div class="btn_item_edit" id="edit${arr[i].itemId}" data-btn-edit="${arr[i].itemId}">
+                    //     <img src="icons/edit.png" alt="">
+                    // </div>
+        );  
+        }  
+    }
 
-            
-        }
+    currentItemAfterEvent (dataAtr) {
         
+        this.getDataFromLocalStorage ("arrItems");
+        let buttonData = event.currentTarget.getAttribute(dataAtr);            
+        return this.arrItems.find(item => item.itemId == buttonData);
         
     }
+
+    fillingFieldsBySelectors (name, price, descr, currentItem) {
+        $(name).html(currentItem.itemName);
+        $(price).html(currentItem.itemPrice);
+        $(descr).html(currentItem.itemDescription);
+
+    }
+
     
+    
+
+    showItem () {
+          
+        $(".item_btn_more_info").on("click", (event) => {
+            $("#info_text_place_id").html(` <ul>
+            <li class="item_name" id="item_name_info_id"></li>
+            <li class="item_category">&#9734; <span id="item_category_info_id"></span></li>
+            <li class="item_price" id="item_price_info_id"></li>
+            <li class="item_descr" id="item_descr_modal_show"></li>
+        </ul> `
+            );
+            
+            this.openModalWinWithOutEvent($("#modal_win_info_id"));
+
+            let currentItem = this.currentItemAfterEvent("data-btn-open");
+            $("#modal_item_edit_btn").attr('data-btn-open', currentItem.itemId);
+            $("#modal_item_delete_btn").attr('data-btn-open', currentItem.itemId);
+
+            this.fillingFieldsBySelectors ( 
+            "#item_name_info_id",  
+            "#item_price_info_id", 
+            "#item_descr_modal_show",
+            currentItem);
+            $("#item_img_info_id img").attr('src', currentItem.itemImg);
+            $("#item_category_info_id").html(currentItem.itemCategory);
+            event.stopPropagation();
+
+            this.editItem ();
+           })    
+    }
+
+    editItem () {
+        $("#modal_item_edit_btn").on("click", (event) => {
+            
+
+            $("#info_text_place_id").html(
+                `<select name="category" id="edit_item_select_category">
+                <option value="Cats">Котики</option>
+                <option value="Dogs">Собачки</option>
+                <option value="Birds">Пташечки</option>
+                <option value="Other">Інші</option>
+              </select>
+            <input type="text" name="name" id="edit_item_name" placeholder="Назва">
+            <input type="text" name="price" id="edit_item_price" placeholder="Price">
+            <input type="text" name="url" id="edit_item_img_url" placeholder="Image URL 500x500" value="img/tit1.jpg">
+            <textarea name="description" id="edit_item_description" cols="20" rows="10" placeholder="Опис"></textarea>
+            <button id="edit_item_btn">НАДІСЛАТИ</button>`);
+
+            let currentItem = this.currentItemAfterEvent("data-btn-open");
+            $("#edit_item_btn").attr('data-btn-open', currentItem.itemId);
+            
+
+            console.log(this.arrItems);
+            console.log(currentItem);
+
+            $("#edit_item_select_category").val(currentItem.itemCategory);
+            $("#edit_item_name").val(currentItem.itemName);
+            $("#edit_item_price").val(currentItem.itemPrice);
+            $("#edit_item_img_url").val(currentItem.itemImg);
+            $("#edit_item_description").val(currentItem.itemDescription);
+
+            $("#edit_item_img_url").on("change", (event) => {
+                $("#item_img_info_id img").attr('src', $("#edit_item_img_url").val());
+            })
+            
+
+            $('#your_id [value=3]').attr('selected', 'true');
+
+            $("#edit_item_btn").on("click", (event) => {
+                let itemId = currentItem.itemId;
+                let itemName = $("#edit_item_name").val();
+                let itemCategory = $("#edit_item_select_category").val();
+                let itemPrice = $("#edit_item_price").val();
+                let itemImg = $("#edit_item_img_url").val();;
+                let itemDescription = $("#edit_item_description").val();
+
+                let newObj = {itemId, itemName, itemCategory, itemPrice, itemImg, itemDescription};
+                console.log(newObj);
+
+                this.arrItems = this.arrItems.map(obj => {
+                    if (obj.itemId === newObj.itemId) {
+                      return newObj;
+                    }
+                    return obj;
+                  });
+
+                  this.setDataToLocalStorage ("arrItems", this.arrItems);
+                  this.getDataFromLocalStorage ("arrItems");
+
+                });
+
+                
+
+            
+
+
+
+
+           
+
+        }
+
+        )}
+
+    
+        
+
+    
+
 }
 
 
-const shop = new Shop("Shop", [], []);                                                 //Створення магазину
+const shop = new Shop("Shop", [], []);                                             //Створення магазину
+const items = new Items();                                                         //Створення экземпляру классу Items для виклику методів
 shop.shopTitle();                                                                  //Назва магазину
 shop.getDataFromLocalStorage ("arrItems");                                         //Актуальний массив данних з Local Storage                              
 shop.openModalWindowByButton( $("#btn_add_item"), $("#modal_item_add_edit") );     //Відкриття для редагування
 shop.openModalWindowByButton( $("#btn_filter"), $("#modal_filter_id") );
-
 shop.closeModalWindows();   
 
-shop.formData();
+shop.formData();                                                                   //Функція створення нового товару
+shop.parseArrToClassItems (shop.arrItems);                                         //Arr of Items from Arr
+//
+console.log("Local Storage in: ");
 console.log(shop.arrItems);
-shop.parseArrToClassItems (shop.arrItems);
+console.log("Arr of Items: ");
 console.log(shop.arrItemsObj);
-let items = new Items();
+
+
+//Load Catalog
 items.loadCatalog (shop.arrItemsObj);
 
 //After Catalog Upload
-shop.openModalWindowByButton( $(".btn_item_edit"), $("#modal_item_add_edit") );            //Edit Button                                   
+// shop.openModalWindowByButton( $(".btn_item_edit"), $("#modal_item_add_edit") );            //Edit Button                                 
 
+shop.showMessage(`Кількість товарів в Local Storage - 
+    ${shop.arrItems.length} шт \n
+     на сумму ${shop.calcSum()} грн`
+    , 2000);
+
+items.showItem();
+items.editItem();
